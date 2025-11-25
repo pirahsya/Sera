26293a4dc19e16450d65f471ce60075db8f71b41
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-26 03:42:31 +0700

    feat: enhance Sidebar with state, context, and new UI layout

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAppContext } from "../context/AppContext";
+// import { assets } from "../assets";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const { chats, setSelectedChat, theme, setTheme, user } = useAppContext();
+  const [search, setSearch] = useState("");
 
-export default Sidebar
+  return (
+    <div className="flex flex-col h-screen min-w-72 p-5 dark:bg-linear-to-b from-[#24216E]/30 to-[#000000]/30 border-r border-[#1E1980]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1">
+      {/* Logo */}
+      <img
+        // src={theme === "dark" ? assets.logo_full : assets.logo_full_dark}
+        alt=""
+        className="w-full max-w-48"
+      />
+    </div>
+  );
+};
+
+export default Sidebar;
