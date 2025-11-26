0ccbe8608fd753c4e101676daed92d7a83bd8ec2
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-26 15:41:24 +0700

    feat: add message bubble rendering to Message component

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const Message = () => {
+const Message = ({ message }) => {
   return (
-    <div>Message</div>
-  )
-}
+    <div>
+      {message.role === "user" ? (
+        <div className="flex justify-end my-4 gap-2">
+          <div className="flex flex-col gap-2 p-2 px-4 bg-slate-50 dark:bg-[#241E80]/30 border border-[#1E1980]/30 rounded-md max-w-2xl">
+            <p className="text-sm dark:text-primary">{message.content}</p>
+          </div>
+        </div>
+      ) : (
+        <div className="inline-flex flex-col gap-2 p-2 px-4 max-w-2xl bg-primary/20 dark:bg-[#241E80]/30 border border-[#1E1980]/30 rounded-md my-4">
+          {message.isImage ? (
+            <img
+              src={message.content}
+              alt=""
+              className="w-full max-w-md mt-2 rounded-md"
+            />
+          ) : (
+            <div className="text-sm dark:text-primary reset-tw">
+              {message.content}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Message
+export default Message;
