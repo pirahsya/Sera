aa24b9b2cd9fa2669599ed332a9a9eab32ede961
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-27 04:13:17 +0700

    feat(chat): add chat schema and model for storing user conversations

---
+++ server/models/Chat.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ChatSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    userName: { type: String, required: true },
+    name: { type: String, required: true },
+    message: [
+      {
+        isImage: { type: Boolean, required: true },
+        role: { type: String, required: true },
+        content: { type: String, required: true },
+        timestamp: { type: Number, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Chat = mongoose.model("Chat", ChatSchema);
+
+export default Chat;
