a161c57512a11080e6f110950f7b78f6fac31c86
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-26 14:23:44 +0700

    feat: add mobile menu trigger to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import ChatBox from "./components/ChatBox";
 import Credit from "./pages/Credit";
 import Library from "./pages/Library";
+import { Menu } from "lucide-react";
 
 const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
+      {!isMenuOpen && (
+        <Menu
+          onClick={() => setIsMenuOpen(true)}
+          size={20}
+          className="absolute top-3 left-3 cursor-pointer md:hidden text-gray-600 dark:text-white"
+        />
+      )}
       <div className="dark:bg-linear-to-b from-[#24216E] to-[#000000] dark:text-white">
         <div className="flex h-screen w-screen">
-          <Sidebar />
+          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           <Routes>
             <Route path="/" element={<ChatBox />} />
             <Route path="/kredit" element={<Credit />} />
