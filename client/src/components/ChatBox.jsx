782b85eb0c135aecb2f0faeacd4e1479d45791a7
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-26 17:35:47 +0700

    feat: add auto-scroll behavior to ChatBox UI

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useAppContext } from "../context/AppContext";
 import Message from "./Message";
 import { SendHorizontal, Square } from "lucide-react";
 
 const ChatBox = () => {
+  const containerRef = useRef(null);
+
   const { selectedChat, theme } = useAppContext();
 
   const [messages, setMessages] = useState([]);
@@ -21,10 +23,19 @@ const ChatBox = () => {
     }
   }, [selectedChat]);
 
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
+
   return (
     <div className="flex-1 flex flex-col justify between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
       {/* Chat Messages */}
-      <div className="flex-1 mb-5 overflow-y-scroll">
+      <div ref={containerRef} className="flex-1 mb-5 overflow-y-scroll">
         {messages.length === 0 && (
           <div className="h-full flex items-center justify-center text-primary">
             <p className="text-4xl sm:text-6xl text-center text-gray-400 dark:text-white">
