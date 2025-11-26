56028f276247b794e15d414f49f0a85aa7073375
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-26 15:33:07 +0700

    feat: add chat messages rendering to ChatBox UI

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import Message from "./Message";
 
 const ChatBox = () => {
-  return (
-    <div>ChatBox</div>
-  )
-}
+  const { selectedChat, theme } = useAppContext();
 
-export default ChatBox
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (selectedChat) {
+      setMessages(selectedChat.messages);
+    }
+  }, [selectedChat]);
+
+  return (
+    <div className="flex-1 flex flex-col justify between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40">
+      {/* Chat Messages */}
+      <div className="flex-1 mb-5 overflow-y-scroll">
+        {messages.length === 0 && (
+          <div className="h-full flex items-center justify-center text-primary">
+            <p className="text-4xl sm:text-6xl text-center text-gray-400 dark:text-white">
+              Tanya apa saja
+            </p>
+          </div>
+        )}
+
+        {messages.map((message, index) => (
+          <Message key={index} message={message} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ChatBox;
