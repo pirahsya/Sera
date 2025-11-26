17fa6be7a0c1481e05e1256d88a01edbb7ea8f48
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-26 16:34:41 +0700

    feat: add loading indicator to ChatBox UI

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -29,6 +29,15 @@ const ChatBox = () => {
         {messages.map((message, index) => (
           <Message key={index} message={message} />
         ))}
+
+        {/* Loading */}
+        {loading && (
+          <div className="loader flex items-center gap-1.5">
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+            <div className="w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce"></div>
+          </div>
+        )}
       </div>
     </div>
   );
