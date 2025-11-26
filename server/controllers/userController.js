d43391bf5566c945a1125de86201ae2069395015
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-27 03:25:34 +0700

    feat(auth): add user auth controllers for register, login, and get user

---
+++ server/controllers/userController.js
@@ -0,0 +1,64 @@
+import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+// API to register user
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.json({ success: false, message: "Email sudah terdaftar" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    const token = generateToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// API to login user
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (isMatch) {
+        const token = generateToken(user._id);
+        return res.json({ success: true, token });
+      }
+    }
+
+    return res.json({ success: false, message: "Email atau password salah" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get user data
+export const getUser = async (req, res) => {
+  try {
+    const user = req.user;
+
+    return res.json({ success: true, user });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
