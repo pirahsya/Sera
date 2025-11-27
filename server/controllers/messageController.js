cac5cc5d59af7cf9ee77dcf446f66edc3e965fdd
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-27 18:02:17 +0700

    feat(message-controller): integrate OpenAI config and finalize message handlers

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import Chat from "../models/Chat.js";
 import User from "../models/User.js";
 import imagekit from "../configs/imagekit.js";
+import openai from "../configs/openai.js";
 
 // API to send a text message to AI
 export const textMessageController = async (req, res) => {
