2b6af87acd38a290a10ea5079822cdf2faddf880
pirahsya <166244778+pirahsya@users.noreply.github.com>
2025-11-28 20:51:55 +0700

    chore(env): rename VITE_BASE_URL to VITE_SERVER_URL
    Update axios baseURL configuration

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL;
 
 const AppContext = createContext();
 
